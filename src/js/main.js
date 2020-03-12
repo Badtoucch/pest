ba6b26dc3f1ebd2fb8c3738902210dcad93bfe76
Vladislad T <v.trundenko@everad.com>
2020-03-12 11:32:47 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import "./helpers/postDate";
+import scrollSmooth from "./helpers/smooth-scroll.js";
+// import example from './modules/example.js';
+
+import people from "./modules/people.js";
+import footer from "./modules/footer.js";
+
+function main() {
+	people();
+  footer();
+}
+
+main();
