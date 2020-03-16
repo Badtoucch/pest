704c9b7c61fc61e8e35bfc28cfcba2082b68caa2
Vladislad T <v.trundenko@everad.com>
2020-03-16 16:52:04 +0200

    vlad

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,14 @@
 import "./helpers/postDate";
 import scrollSmooth from "./helpers/smooth-scroll.js";
 // import example from './modules/example.js';
-
+import menu from "./modules/menu.js";
 import people from "./modules/people.js";
 import footer from "./modules/footer.js";
 
 function main() {
 	people();
-  footer();
+	menu();
+	footer();
 }
 
 main();
